8c8fb9b7e4eb7afd00329e82633a636967a483b1
James Montilla <montivive@hotmail.com>
2023-11-03 18:10:17 +1000

    Last twicks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
+import { useState } from 'react';
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main className='bg-white  text-black  px-10'>
+    <main className='bg-white  text-black  px-10 md:px-20 lg:px-40'>
       <section className='bg h-screen min-h-screen'>
         <nav className='flex justify-between py-10 mb-12'>
           <h1 className='text-xl'>James Monti</h1>
@@ -34,13 +36,57 @@ export default function Home() {
           <p className='text-md py-5 leading-8 text-gray-800'>Freelancer providing services for design and development needs. Let's get cracking.Freelancer providing services for design and development needs. Let's get cracking. <span className='text-teal-500'>Some highlighted text</span> and then more text here to fill</p>
           <p className='text-md py-5 leading-8 text-gray-800'>Freelancer providing services for design and development needs. Let's get cracking.Freelancer providing services for design and development needs. Let's get cracking. and then more text here to fill</p>
         </div>
+        <div className='lg:flex gap-10'>
+          <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='h-40 w-40 mx-auto bg-red-400'>Image need to put in public and then import as per the icons</div> 
+            <h3 className='text-lg font-medium pt-8 pb-2'>Beauti designs</h3>
+            <p>
+              Creating elegant stuff for smart people
+            </p>
+            <h4 className='py-4 text-teal-600'>Design Tools I use:</h4>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+          </div>
+          <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='h-40 w-40 mx-auto bg-orange-400'>Image need to put in public and then import as per the icons</div> 
+            <h3 className='text-lg font-medium pt-8 pb-2'>Beauti designs</h3>
+            <p>
+              Creating elegant stuff for smart people
+            </p>
+            <h4 className='py-4 text-teal-600'>Design Tools I use:</h4>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+          </div>
+          <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className='h-40 w-40 mx-auto bg-blue-400'>Image need to put in public and then import as per the icons</div> 
+            <h3 className='text-lg font-medium pt-8 pb-2'>Beauti designs</h3>
+            <p>
+              Creating elegant stuff for smart people
+            </p>
+            <h4 className='py-4 text-teal-600'>Design Tools I use:</h4>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+            <p className='text-gray-800 py-1'>Photoshop</p>
+          </div>
+        </div>
+      </section>
+      <section>
         <div>
-        <div className='h-40 w-full bg-red-400'>
-          
-         <span>Image need to put in public and then import as per the icons</span> 
+          <h3 className='text-3xl py-1'>Portfolio</h3>
+          <p className='text-md py-5 leading-8 text-gray-800'>Freelancer providing services for design and development needs. Let's get cracking.Freelancer providing services for design and development needs. Let's get cracking. <span className='text-teal-500'>Some highlighted text</span> and then more text here to fill</p>
+          <p className='text-md py-5 leading-8 text-gray-800'>Freelancer providing services for design and development needs. Let's get cracking.Freelancer providing services for design and development needs. Let's get cracking. and then more text here to fill</p>
+        </div>
+        <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+          <div className='h-80 w-full  bg-red-400 base-1/3 flex-1'>Image need to put in public and then import as per the icons</div>
+          <div className='h-80 w-full  bg-red-400 base-1/3 flex-1'>Image need to put in public and then import as per the icons</div>
+          <div className='h-80 w-full  bg-red-400 base-1/3 flex-1'>Image need to put in public and then import as per the icons</div>
+          <div className='h-80 w-full  bg-red-400 base-1/3 flex-1'>Image need to put in public and then import as per the icons</div>
+        </div>
+
         
-        </div>
-        </div>
+
       </section>
     </main>
   )
